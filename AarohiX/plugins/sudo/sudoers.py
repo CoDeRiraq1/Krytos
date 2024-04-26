45dd708fa5769774c196cfb048d157fbffed85e8
CoDeRiraq1 <160813055+CoDeRiraq1@users.noreply.github.com>
2024-04-26 20:49:09 +0300

    Update sudoers.py

--- AarohiX/plugins/sudo/sudoers.py
+++ AarohiX/plugins/sudo/sudoers.py
@@ -10,7 +10,7 @@ from AarohiX.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 from strings.filters import command
 
-@app.on_message(command(["رفع منشئ"]) & filters.user(OWNER_ID))
+@app.on_message(command(["رفع منشئ" ,"رفع مطور" ]) & filters.user(OWNER_ID))
 @language
 async def useradd(client, message: Message, _):
     if not message.reply_to_message:
@@ -27,7 +27,7 @@ async def useradd(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-@app.on_message(command(["حذف منشئ", "rmsudo"]) & filters.user(OWNER_ID))
+@app.on_message(command(["حذف مطور", "rmsudo"]) & filters.user(OWNER_ID))
 @language
 async def userdel(client, message: Message, _):
     if not message.reply_to_message:
@@ -46,9 +46,9 @@ async def userdel(client, message: Message, _):
 
 GAMDOP = "https://te.legra.ph/file/f7e1acf9338cc453a87ad.jpg"
 
-@app.on_message(command(["المنشئين", "المنشئيين", "sudoers"]) & ~BANNED_USERS)
+@app.on_message(command(["المطورين", "المنشئيين", "sudoers"]) & ~BANNED_USERS)
 async def sudoers_list(client, message: Message):
-    keyboard = [[InlineKeyboardButton(" - المنشئين ", callback_data="check_sudo_list")]]
+    keyboard = [[InlineKeyboardButton(" - المطورين ", callback_data="check_sudo_list")]]
     reply_markups = InlineKeyboardMarkup(keyboard)
     await message.reply_photo(photo=GAMDOP, caption="~ اهلا عزيزي المطور .\n\n~ لرؤية قائمة منشئين البوت .\n\n~ اضغط على زر المطورين . ", reply_markup=reply_markups)
 
