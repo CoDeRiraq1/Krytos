535393954866712ad6c3ebcc2d30e91730b4344c
CoDeRiraq1 <160813055+CoDeRiraq1@users.noreply.github.com>
2024-05-01 00:14:24 +0300

    Update Obaida.py

--- AarohiX/plugins/play/Obaida.py
+++ AarohiX/plugins/play/Obaida.py
@@ -9,8 +9,8 @@ import random
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from AnonX import app
+from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AarohiX import app
 from random import  choice, randint
 @app.on_message(
     command(["المطور", "سورس", "السورس", "سورس ميكسثون",  "ميكسثون"])
