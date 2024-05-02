10d968567228a5dfca48d3f8f9d3834025ceead1
CoDeRiraq1 <160813055+CoDeRiraq1@users.noreply.github.com>
2024-05-02 17:35:53 +0300

    Update call.py

--- AarohiX/core/call.py
+++ AarohiX/core/call.py
@@ -1,16 +1,23 @@
 import asyncio
-import os
-import pytgcalls 
-
 from datetime import datetime, timedelta
 from typing import Union
+
 from pyrogram import Client
+from pyrogram.errors import (
+    ChatAdminRequired,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
 from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls, StreamType
-from pytgcalls.exceptions import (
-    AlreadyJoinedError,
-    NoActiveGroupCall,
-    TelegramServerError,
+from pytgcalls import PyTgCalls
+from ntgcalls import TelegramServerError
+from pytgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall
+from pytgcalls.types import (
+    JoinedGroupCallParticipant,
+    LeftGroupCallParticipant,
+    MediaStream,
+    Update,
+     
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
