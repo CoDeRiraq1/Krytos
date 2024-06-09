3f5f65c07182ec05f8785e2a250e95a90afc3633
CoDeRiraq1 <160813055+CoDeRiraq1@users.noreply.github.com>
2024-06-09 04:59:15 +0300

    Update Whatsapp.py

--- AarohiX/plugins/Management/Whatsapp.py
+++ AarohiX/plugins/Management/Whatsapp.py
@@ -2,14 +2,14 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from AarohiX import app 
 
-@app.on_message(filters.command("WhatsApp"))
+@app.on_message(filters.command("Whats"))
 async def generate_whatsapp_link(client, message: Message):
     if len(message.command) < 2:
-        await message.reply("Please enter your phone number after the command. Example: /WhatsApp +1234567890")
+        await message.reply("ارسل رقم الهاتف مع الأمر. الأستخدام: /Whats +1234567890")
         return
 
     phone_number = message.command[1]
 
     whatsapp_link = f"https://wa.me/{phone_number}"
 
-    await message.reply(f"Click the link to open WhatsApp with the number {phone_number}:\n{whatsapp_link}")
+    await message.reply(f"اضغط على الرابط لمراسة  {phone_number}:\n{whatsapp_link}")
